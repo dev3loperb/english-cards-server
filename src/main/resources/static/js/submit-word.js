d4730195dad382cacf4bb0d1c5d29536a98c9b78
Igor Pergenitsa <ipergenitsa@thumbtack.net>
2017-07-06 13:01:40 +0600

    all words page showing all words which are in db

---
+++ src/main/resources/static/js/submit-word.js
@@ -0,0 +1,7 @@
+$("#addWordForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault()
+    var original = $("#original_word").val()
+    var translation = $("#translation").val()
+    addWord(original, translation)
+});

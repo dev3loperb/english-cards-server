88928d0632a3f8ccced707af4d905e5ae79e9909
Igor Pergenitsa <ipergenitsa@gmail.com>
2017-07-18 18:53:41 +0600

    validate word from api it's non empty original and translation fields

--- src/main/java/com/github/ipergenitsa/engcards/controller/api/WordApiController.java
+++ src/main/java/com/github/ipergenitsa/engcards/controller/api/WordApiController.java
@@ -1,11 +1,13 @@
 package com.github.ipergenitsa.engcards.controller.api;
 
+import com.github.ipergenitsa.engcards.controller.api.rest.RestWord;
 import com.github.ipergenitsa.engcards.entity.Word;
 import com.github.ipergenitsa.engcards.repository.WordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sun.plugin.dom.exception.InvalidStateException;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
@@ -15,8 +17,7 @@ public class WordApiController {
     private WordRepository wordRepository;
 
     @PostMapping("/add-word")
-    public String addWord(@RequestBody RestWord restWord) {
-        // TODO check restWord has data
+    public String addWord(@Valid @RequestBody RestWord restWord) {
         final Word wordToSave = new Word();
         wordToSave.setOriginal(restWord.getOriginal());
         wordToSave.setTranslation(restWord.getTranslation());
@@ -29,33 +30,5 @@ public class WordApiController {
     @GetMapping("/word-all")
     public Collection<Word> getAll() {
         return wordRepository.findAll();
-    }
-
-    private static class RestWord {
-        private String original;
-        private String translation;
-
-        public RestWord() {}
-
-        public RestWord(String original, String translation) {
-            this.original = original;
-            this.translation = translation;
-        }
-
-        public String getOriginal() {
-            return original;
-        }
-
-        public String getTranslation() {
-            return translation;
-        }
-
-        public void setOriginal(String original) {
-            this.original = original;
-        }
-
-        public void setTranslation(String translation) {
-            this.translation = translation;
-        }
     }
 }

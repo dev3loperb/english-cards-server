b19d2414a19ccee1a3f66eb04fc671d24940a804
Igor Pergenitsa <ipergenitsa@gmail.com>
2017-12-05 21:16:09 +0600

    add crossorigin annotation

--- src/main/java/com/github/ipergenitsa/engcards/controller/api/WordApiController.java
+++ src/main/java/com/github/ipergenitsa/engcards/controller/api/WordApiController.java
@@ -7,6 +7,7 @@ import com.github.ipergenitsa.engcards.controller.api.rest.RestWord;
 import com.github.ipergenitsa.engcards.entity.Word;
 import com.github.ipergenitsa.engcards.repository.WordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +36,7 @@ public class WordApiController {
     }
 
     @GetMapping("/random")
+    @CrossOrigin
     public Word getRandomWord() {
         return wordRepository.findRandom();
     }
